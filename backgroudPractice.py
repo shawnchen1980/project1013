96fd4892ca08b97a0a95bbcb950a573cf2245e5f
ShawnChen <shawnchen1980@hotmail.com>
2019-10-25 21:59:39 +0800

    updated

---
+++ backgroudPractice.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 20 09:29:34 2019
+
+@author: shawn
+"""
+
+import cv2
+
+camera = cv2.VideoCapture(1) # 参数0表示第一个摄像头
+mog = cv2.createBackgroundSubtractorMOG2()
+
+while (1):
+    grabbed, frame_lwpCV = camera.read()
+    fgmask = mog.apply(frame_lwpCV)
+    cv2.imshow('frame', fgmask)
+    key = cv2.waitKey(1) & 0xFF
+    # 按'q'健退出循环
+    if key == ord('q'):
+        break
+camera.release()
+cv2.destroyAllWindows()
