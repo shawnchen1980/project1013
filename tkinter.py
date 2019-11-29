b801fcf82094fdc6b2acad6fbddeba6f2b61906b
Shawnchen <shawnchen1980@hotmail.com>
2019-11-29 14:08:37 +0800

    updated

--- tkinter.py
+++ tkinter.py
@@ -7,6 +7,9 @@ Created on Fri Nov 29 11:02:44 2019
 
 import tkinter
 import cv2
+import PIL.Image, PIL.ImageTk
+import time
+
 class MyVideoCapture:
     def __init__(self, video_source=0):
         # Open the video source
@@ -45,7 +48,26 @@ class App:
         self.canvas = tkinter.Canvas(window, width = self.vid.width, height = self.vid.height)
         self.canvas.pack()
         
+        self.btn_snapshot=tkinter.Button(window, text="Snapshot", width=50, command=self.snapshot)
+        self.btn_snapshot.pack(anchor=tkinter.CENTER, expand=True)
+        
+        self.delay = 15
+        self.update()
+        
         self.window.mainloop()
- 
+     def update(self):
+        ret, frame = self.vid.get_frame()
+
+        if ret:
+            self.photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(frame))
+            self.canvas.create_image(0, 0, image = self.photo, anchor = tkinter.NW)
+
+        self.window.after(self.delay, self.update)
+     def snapshot(self):
+         ret, frame = self.vid.get_frame()
+         if ret:
+             cv2.imwrite("frame-" + time.strftime("%d-%m-%Y-%H-%M-%S") + ".jpg", cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
+             
+
  # Create a window and pass it to the Application object
 App(tkinter.Tk(), "Tkinter and OpenCV")
