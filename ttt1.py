e4149ef1112664dd8087f1038fd7c42e9985c430
unknown <shawnchen1980@hotmail.com>
2019-12-14 14:11:47 +0800

    ttt1

--- ttt1.py
+++ ttt1.py
@@ -7,6 +7,7 @@
 """
 opencv-with-tkinter.py:
 https://www.pyimagesearch.com/2016/05/23/opencv-with-tkinter/
+https://www.python-course.eu/tkinter_entry_widgets.php
 不需要
 pip install image
 """
@@ -98,7 +99,9 @@ def videoLoop():
             # have a maximum width of 300 pixels
             frame = vs.read()
             frame = imutils.resize(frame, width=600)
-
+            frame = cv2.flip(frame, 1)
+            cv2.rectangle(frame, (0, 0),
+                  (int(0.5 * frame.shape[1]), int(0.8 * frame.shape[0])), (255, 0, 0), 2)
             #  represents images in BGR order; however PIL
             # represents images in RGB order, so we need to swap
             # the channels, then convert to PIL and ImageTk format
@@ -111,7 +114,7 @@ def videoLoop():
                 panelA = Label(rFrame,image=image)
                 panelA.image = image
 #                panelA.pack(side="left", padx=10, pady=10)
-                panelA.grid(row=0,column=0)
+                panelA.grid(row=0,column=0,padx=50)
 
             # otherwise, simply update the panel
             else:
@@ -138,9 +141,11 @@ def videoLoop():
         print("[INFO] caught a RuntimeError")
 # initialize the window toolkit along with the two image panels
 root = Tk()
+root.title("手语识别教学软件")
 threadStop=False
 panelA = None
 panelB = None
+imglbl=None
 rFrame=None
 vs=None
 selected = IntVar()
@@ -151,21 +156,34 @@ checked2.set(True)
 # create a button, then when pressed, will trigger a file chooser
 # dialog and allow the user to select an input image; then add the
 # button the GUI
+def readImage(path,size):
+    image = cv2.imread(path)
+    image=cv2.resize(image,size)
+    image = Image.fromarray(image)
+    image = ImageTk.PhotoImage(image)
+    return image
+    
 def showQuestion():
-#    global root
+    global imglbl
     lFrame=Frame(root)
     lFrame.grid(row=0,column=0)
-    lbl=Label(lFrame,text="含义:",font=("宋体",25),borderwidth=2, relief="groove",width=30,anchor=W,padx=50)
-    lbl.grid(row=0,column=0,sticky=W+N,padx=10)
-    image = cv2.imread("22.jpg")
+    lbl=Label(lFrame,text="含义:",font=("宋体",25),borderwidth=2, relief="groove",width=5,anchor=W,padx=50)
+    lbl.grid(row=0,column=0,sticky=W+N,padx=10,pady=10)
+    e1=Entry(lFrame,font= "宋体 25 bold",width=14)
+    e2=Entry(lFrame,font=("宋体",25),width=14)
+    e1.grid(row=0,column=1)
+    e2.grid(row=2,column=1)
+    image = cv2.imread("pic/a.jpg")
     image=cv2.resize(image,(224,224))
     image = Image.fromarray(image)
     image = ImageTk.PhotoImage(image)
-    lbl = Label(lFrame,image=image,compound=RIGHT,text="图片:",font=("宋体",25),borderwidth=2, relief="groove",anchor=W,padx=50)
-    lbl.image = image
+    lbl = Label(lFrame,text="图片:",font=("宋体",25),borderwidth=2, relief="groove",anchor=W,padx=50)
     lbl.grid(row=1,column=0,sticky=W,padx=10)
-    lbl=Label(lFrame,text="提示:",font=("宋体",25),borderwidth=2, relief="groove",width=30,anchor=W,padx=50)
-    lbl.grid(row=2,column=0,sticky=W+S,padx=10)
+    imglbl = Label(lFrame,image=image,borderwidth=2, relief="groove",anchor=W,padx=50)
+    imglbl.image = image
+    imglbl.grid(row=1,column=1,sticky=W,padx=10)
+    lbl=Label(lFrame,text="提示:",font=("宋体",25),borderwidth=2, relief="groove",width=5,anchor=W,padx=50)
+    lbl.grid(row=2,column=0,sticky=W+S,padx=10,pady=10)
 def showNavBar():
     bFrame=Frame(root)
     bFrame.grid(row=1,column=0,columnspan=2)
@@ -173,22 +191,26 @@ def showNavBar():
     btn.grid(row=0,column=0)
     btn=Button(bFrame,text="下一题",font=("宋体",15))
     btn.grid(row=0,column=2)
+    btn=Button(bFrame,text="设置背景",font=("宋体",15))
+    btn.grid(row=1,column=0)
+    btn=Button(bFrame,text="提交",font=("宋体",15))
+    btn.grid(row=1,column=2)
     combo=ttk.Combobox(bFrame)
     combo['values']=(1,2,3,4,"A","B","C")
     combo.grid(row=0,column=1)
     rad1 = Radiobutton(bFrame,text='做手势', value=1, variable=selected, command=showSelectedMode)
     rad2 = Radiobutton(bFrame,text='选图片', value=2, variable=selected, command=showSelectedMode)
     rad3 = Radiobutton(bFrame,text='选含义', value=3, variable=selected, command=showSelectedMode)
-    rad1.grid(row=1,column=0)
-    rad2.grid(row=1,column=1)
-    rad3.grid(row=1,column=2)
+    rad1.grid(row=2,column=0)
+    rad2.grid(row=2,column=1)
+    rad3.grid(row=2,column=2)
     chk=Checkbutton(bFrame,text='显示含义',var=checked1, command=showSelectedMode)
-    chk.grid(row=2,column=0)
+    chk.grid(row=3,column=0)
     chk=Checkbutton(bFrame,text='显示图片',var=checked2, command=showSelectedMode)
-    chk.grid(row=2,column=1)
+    chk.grid(row=3,column=1)
     
 def showSelectedMode():
-    global rFrame
+    global rFrame,imglbl
     if not(vs is None):
         stopVideo()
         time.sleep(1)
@@ -203,6 +225,15 @@ def showSelectedMode():
         showPicOptions()
     else:
         showVideo()
+    if((not (checked2.get())) and (not (imglbl is None))):
+        image=readImage("pic/question.jpg",(224,224))
+        imglbl.configure(image=image)
+        imglbl.image=image
+    else:
+        image=readImage("pic/a.jpg",(224,224))
+        imglbl.configure(image=image)
+        imglbl.image=image
+        
     print(checked1.get())
     print(checked2.get())
 
@@ -210,24 +241,33 @@ def showTextOptions():
     global rFrame
     rFrame=Frame(root)
     rFrame.grid(row=0,column=1)
-    rad1 = Radiobutton(rFrame,text='做手势', value=1)
-    rad2 = Radiobutton(rFrame,text='选图片', value=2)
-    rad3 = Radiobutton(rFrame,text='选含义', value=3)
-    rad1.grid(row=0,column=0)
+    rad1 = Radiobutton(rFrame,text='字母A',font=("宋体",25), value=1)
+    rad2 = Radiobutton(rFrame,text='字母B',font=("宋体",25), value=2)
+    rad3 = Radiobutton(rFrame,text='字母C',font=("宋体",25), value=3)
+    rad4 = Radiobutton(rFrame,text='字母D',font=("宋体",25), value=4)
+    rad1.grid(row=0,column=0,padx=50)
     rad2.grid(row=1,column=0)
     rad3.grid(row=2,column=0)
-
+    rad4.grid(row=3,column=0)
 def showPicOptions():
     global rFrame
     rFrame=Frame(root)
     rFrame.grid(row=0,column=1)
-    btn=Button(rFrame,text="hello1")
+    image = readImage("pic/a.jpg",(112,112))
+    btn=Radiobutton(rFrame,image=image,value=1)
+    btn.image=image
     btn.grid(row=0,column=0)
-    btn=Button(rFrame,text="hello2")
+    image=readImage("pic/b.jpg",(112,112))
+    btn=Radiobutton(rFrame,image=image,value=2)
+    btn.image=image
     btn.grid(row=0,column=1)
-    btn=Button(rFrame,text="hello3")
+    image=readImage("pic/c.jpg",(112,112))
+    btn=Radiobutton(rFrame,image=image,value=3)
+    btn.image=image
     btn.grid(row=1,column=0)
-    btn=Button(rFrame,text="hello4")
+    image=readImage("pic/d.jpg",(112,112))
+    btn=Radiobutton(rFrame,image=image,value=4)
+    btn.image=image
     btn.grid(row=1,column=1)
 
         
